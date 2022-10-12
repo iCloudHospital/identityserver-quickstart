b2d38185ad23993ecca45b151c5bc6fd4495c509
hyounoo <9585318+hyounoo@users.noreply.github.com>
2022-10-12 19:29:58 +0900

    4_EntityFramework Completed

--- 4_EntityFramework/src/IdentityServer/HostingExtensions.cs
+++ 4_EntityFramework/src/IdentityServer/HostingExtensions.cs
@@ -1,23 +1,69 @@
 using Duende.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Duende.IdentityServer.EntityFramework.DbContexts;
+using Duende.IdentityServer.EntityFramework.Mappers;
 using Serilog;
 
 namespace IdentityServer;
 
 internal static class HostingExtensions
 {
+  private static void InitializeDatabase(IApplicationBuilder app)
+  {
+    using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+    {
+      serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+      var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+      context.Database.Migrate();
+      if (!context.Clients.Any())
+      {
+        foreach (var client in Config.Clients)
+        {
+          context.Clients.Add(client.ToEntity());
+        }
+        context.SaveChanges();
+      }
+
+      if (!context.IdentityResources.Any())
+      {
+        foreach (var resource in Config.IdentityResources)
+        {
+          context.IdentityResources.Add(resource.ToEntity());
+        }
+        context.SaveChanges();
+      }
+
+      if (!context.ApiScopes.Any())
+      {
+        foreach (var resource in Config.ApiScopes)
+        {
+          context.ApiScopes.Add(resource.ToEntity());
+        }
+        context.SaveChanges();
+      }
+    }
+  }
+
   public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
   {
+    var migrationsAssembly = typeof(Program).Assembly.GetName().Name;
+    const string connectionString = @"Data Source=Duende.IdentityServer.Quickstart.EntityFramework.db";
+
     // uncomment if you want to add a UI
     builder.Services.AddRazorPages();
 
-    builder.Services.AddIdentityServer(options =>
+    builder.Services.AddIdentityServer()
+        .AddConfigurationStore(options =>
         {
-          // https://docs.duendesoftware.com/identityserver/v6/fundamentals/resources/api_scopes#authorization-based-on-scopes
-          options.EmitStaticAudienceClaim = true;
+          options.ConfigureDbContext = b => b.UseSqlite(connectionString,
+              sql => sql.MigrationsAssembly(migrationsAssembly));
         })
-        .AddInMemoryIdentityResources(Config.IdentityResources)
-        .AddInMemoryApiScopes(Config.ApiScopes)
-        .AddInMemoryClients(Config.Clients)
+        .AddOperationalStore(options =>
+        {
+          options.ConfigureDbContext = b => b.UseSqlite(connectionString,
+              sql => sql.MigrationsAssembly(migrationsAssembly));
+        })
         .AddTestUsers(TestUsers.Users);
 
     builder.Services.AddAuthentication()
@@ -40,6 +86,8 @@ internal static class HostingExtensions
     {
       app.UseDeveloperExceptionPage();
     }
+
+    InitializeDatabase(app);
 
     // uncomment if you want to add a UI
     app.UseStaticFiles();
--- 4_EntityFramework/src/IdentityServer/Program.cs
+++ 4_EntityFramework/src/IdentityServer/Program.cs
@@ -9,25 +9,28 @@ Log.Information("Starting up");
 
 try
 {
-    var builder = WebApplication.CreateBuilder(args);
+  var builder = WebApplication.CreateBuilder(args);
 
-    builder.Host.UseSerilog((ctx, lc) => lc
-        .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
-        .Enrich.FromLogContext()
-        .ReadFrom.Configuration(ctx.Configuration));
+  builder.Host.UseSerilog((ctx, lc) => lc
+      .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}")
+      .Enrich.FromLogContext()
+      .ReadFrom.Configuration(ctx.Configuration));
 
-    var app = builder
-        .ConfigureServices()
-        .ConfigurePipeline();
-    
-    app.Run();
+  var app = builder
+      .ConfigureServices()
+      .ConfigurePipeline();
+
+  app.Run();
 }
 catch (Exception ex)
 {
+  if (ex.GetType().Name != "StopTheHostException")
+  {
     Log.Fatal(ex, "Unhandled exception");
+  }
 }
 finally
 {
-    Log.Information("Shut down complete");
-    Log.CloseAndFlush();
+  Log.Information("Shut down complete");
+  Log.CloseAndFlush();
 }
