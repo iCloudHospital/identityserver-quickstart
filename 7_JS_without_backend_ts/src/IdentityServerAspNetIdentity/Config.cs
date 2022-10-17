8f8470f9947655c341980273aa4bc4bf2db5f747
Hyounwoo <hyounoosung@gmail.com>
2022-10-17 18:27:42 +0900

    7_JS_without_backend with ts

---
+++ 7_JS_without_backend_ts/src/IdentityServerAspNetIdentity/Config.cs
@@ -0,0 +1,102 @@
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+namespace IdentityServerAspNetIdentity;
+
+public static class Config
+{
+  public static IEnumerable<IdentityResource> IdentityResources =>
+      new IdentityResource[]
+      {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+            new IdentityResource("color", new [] { "favorite_color" })
+      };
+
+  public static IEnumerable<ApiScope> ApiScopes =>
+      new ApiScope[]
+      {
+            new ApiScope("api1", "My API"),
+      };
+
+  public static IEnumerable<Client> Clients =>
+      new Client[]
+      {
+            // machine to machine client
+            new Client
+            {
+                ClientId = "client",
+                ClientSecrets = { new Secret("secret".Sha256()) },
+
+                AllowedGrantTypes = GrantTypes.ClientCredentials,
+                // scopes that client has access to
+                AllowedScopes = { "api1" }
+            },
+
+            // interactive ASP.NET Core Web App
+            new Client
+            {
+                ClientId = "web",
+                ClientSecrets = { new Secret("secret".Sha256()) },
+
+                AllowedGrantTypes = GrantTypes.Code,
+                    
+                // where to redirect to after login
+                RedirectUris = { "https://localhost:5002/signin-oidc" },
+
+                // where to redirect to after logout
+                PostLogoutRedirectUris = { "https://localhost:5002/signout-callback-oidc" },
+
+                AllowOfflineAccess = true,
+
+                AllowedScopes = new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    "api1",
+                    "color"
+                }
+            },
+
+            // JavaScript BFF client
+            new Client
+            {
+                ClientId = "bff",
+                ClientSecrets = { new Secret("secret".Sha256()) },
+
+                AllowedGrantTypes = GrantTypes.Code,
+                
+                // where to redirect to after login
+                RedirectUris = { "https://localhost:5003/signin-oidc" },
+
+                // where to redirect to after logout
+                PostLogoutRedirectUris = { "https://localhost:5003/signout-callback-oidc" },
+
+                AllowedScopes = new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    "api1"
+                }
+            },
+            // JavaScript Client
+            new Client
+            {
+                ClientId = "js",
+                ClientName = "JavaScript Client",
+                AllowedGrantTypes = GrantTypes.Code,
+                RequireClientSecret = false,
+                
+                RedirectUris =           { "https://localhost:5003/callback.html" },
+                PostLogoutRedirectUris = { "https://localhost:5003/index.html" },
+                AllowedCorsOrigins =     { "https://localhost:5003" },
+
+                AllowedScopes = 
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile,
+                    "api1"
+                }
+            }
+      };
+}
